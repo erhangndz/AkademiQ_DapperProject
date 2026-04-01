3180bd059bcde976db1141ece6c626655a99743a
erhangndz <erhangndz@live.com>
2026-04-01 21:54:06 +0300

    updated

--- Furnish.WebUI/Controllers/ProductController.cs
+++ Furnish.WebUI/Controllers/ProductController.cs
@@ -44,5 +44,34 @@ namespace Furnish.WebUI.Controllers
             await _productService.CreateAsync(createProductDto);
             return RedirectToAction("Index");
         }
+
+        public async Task<IActionResult> UpdateProduct(int id)
+        {
+            var categoryList = await _categoryService.GetAllAsync();
+
+            ViewBag.categories = (from category in categoryList
+                                  select new SelectListItem
+                                  {
+                                      Text = category.CategoryName,
+                                      Value = category.Id.ToString()
+                                  }).ToList();
+
+
+            var product = await _productService.GetByIdAsync(id);
+            return View(product);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateProduct(UpdateProductDto updateProductDto)
+        {
+            await _productService.UpdateAsync(updateProductDto);
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> DeleteProduct(int id)
+        {
+            await _productService.DeleteAsync(id);
+            return RedirectToAction("Index");
+        }
     }
 }
